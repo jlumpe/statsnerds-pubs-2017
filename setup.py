dd9c1b1610ad8e09fb3811c028f9d9bff55ed04b
Jared Lumpe <mjlumpe@gmail.com>
2017-10-12 21:21:10 -0700

    Empty package with setup script

---
+++ setup.py
@@ -0,0 +1,15 @@
+"""Setuptools installation script for statsnerds repo."""
+
+from setuptools import setup, find_packages
+
+
+setup(
+	name='statsnerds-pubs-2017',
+	version='0.1',
+	description='Team Stats Nerds pipeline for PUBS 2017',
+	packages=find_packages('statsnerds'),
+	install_requires=[
+		'Biopython>=1.69',
+		'numpy>=1.12'
+	],
+)
---
+++ statsnerds/__init__.py
@@ -0,0 +1,1 @@
+"""Team Stats Nerds pipeline for PUBS 2017."""
