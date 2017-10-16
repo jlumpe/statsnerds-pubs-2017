c322d7db7305f04563033fe8e1943cd4eff96243
Jared Lumpe <mjlumpe@gmail.com>
2017-10-16 15:15:17 -0700

    Include WT asyn sequence as package data file

--- statsnerds/__init__.py
+++ statsnerds/__init__.py
@@ -1,1 +1,10 @@
 """Team Stats Nerds pipeline for PUBS 2017."""
+
+from pkg_resources import resource_string
+
+# Load wild-type sequence
+try:
+	ASYN_WT_SEQ = resource_string(__name__, 'alphasynuclien_wt_seq_dna.txt').decode()
+
+except FileNotFoundError:
+	ASYN_WT_SEQ = None
